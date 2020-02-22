8cf1c4db13fef2bf3e614ab7797b50f9a023ccaa
Ashley Gaskins <ashley.eddings89@gmail.com>
2020-02-22 07:55:25 -0500

    more basilica

--- basilica_service.py
+++ basilica_service.py
@@ -12,6 +12,15 @@ sentences = [
     "I don't think this sentence is very similar at all...",
 ]
 
-with basilica.Connection(BASILICA_API_KEY) as c:
-    embeddings = list(c.embed_sentences(sentences))
-print(embeddings)
+#with basilica.Connection(BASILICA_API_KEY) as c:
+#    embeddings = list(c.embed_sentences(sentences))
+#print(embeddings)
+
+#you could use:
+
+connection = basilica.Connection(BASILICA_API_KEY)
+embeddings = list(connection.embed_sentences(sentences))
+for emb in embeddings:
+   print(type(emb))
+   print(emb)
+   print("-----------")
